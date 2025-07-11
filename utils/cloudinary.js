aebd8f7b0626db6919cfd5bdcb698768e6f7eac3
Ebrahim Salah <159820645+ESalah489@users.noreply.github.com>
2025-07-11 20:38:47 +0300

    Salma branch (#3)
    * feat:Create CRUD for dental services and  integrate advanced search filters and keyword suggestion
    
    * feat:Create CRUD for dental services and  integrate advanced search filters and keyword suggestion
    
    * fix:Edited dortor files
    
    ---------
    
    Co-authored-by: salma <salmazoroub00@gmail.com>

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -14,14 +14,23 @@ export const storage = new CloudinaryStorage({
   params: (req, file) => {
     let folderName = "general";
 
+    if (req.body.type === "doctor") {
+    if (file.fieldname === "profileImage") {
+      folderName = "doctors/profileImages";
+    } else if (file.fieldname === "workImages") {
+      folderName = "doctors/workImages";
+    } else {
+      folderName = "doctors/others";
+    }
+    }
     if (req.body.type === "service") folderName = "services";
-    if (req.body.type === "doctor") folderName = "doctors";
     if (req.body.type === "users") folderName = "users";
 
     return {
       folder: folderName,
     allowed_formats: ["jpg", "png", "jpeg", "webp"],
     transformation: [{ width: 500, height: 500, crop: "limit" }],
+    };
     };
   },
 });
