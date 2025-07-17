b26750870a1461bdcafbb328e977a68f3dbd6ef5
Ebrahim Salah <159820645+ESalah489@users.noreply.github.com>
2025-07-17 20:26:04 +0300

    Salma branch (#6)
    * feat:Create CRUD for dental services and  integrate advanced search filters and keyword suggestion
    
    * feat:Create CRUD for dental services and  integrate advanced search filters and keyword suggestion
    
    * fix:Edited dortor files
    
    * fix:Edited dortor files and merge conflicts
    
    * feat(review clinic and doctor): adding review features for doctor and clinic (#4) (#5)
    
    Co-authored-by: Reem-Yousef <reemyousef20002@gmail.com>
    
    ---------
    
    Co-authored-by: salma <salmazoroub00@gmail.com>
    Co-authored-by: Reem-Yousef <reemyousef20002@gmail.com>

--- src/modules/Search/search.validation.js
+++ src/modules/Search/search.validation.js
@@ -1,8 +1,7 @@
 import Joi from "joi";
 
 export const searchSchema = Joi.object({
-  keyword: Joi.string().min(2).optional(),
-  category: Joi.string().optional(),
+  keyword: Joi.string().min(3).optional(),
   minPrice: Joi.number().optional(),
   maxPrice: Joi.number().optional(),
   sessions: Joi.string().optional(),
@@ -13,9 +12,18 @@ export const searchSchema = Joi.object({
 });
 
 export const searchSuggestionSchema = Joi.object({
-  keyword: Joi.string().min(1).required().messages({
+  keyword: Joi.string().min(3).required().messages({
     "string.base": "Keyword must be a string",
-    "string.min": "Please type at least one letter",
+    "string.min": "Please type at least 3 letter",
     "any.required": "Keyword is required",
   }),
+});
+
+export const searchDoctorsSchema = Joi.object({
+  keyword: Joi.string().min(2).optional(),
+  specialization: Joi.string().optional(),
+  sortBy: Joi.string().valid("createdAt", "experience").optional(),
+  order: Joi.string().valid("asc", "desc").optional(),
+  page: Joi.number().min(1).optional(),
+  limit: Joi.number().min(1).max(100).optional(),
 });
